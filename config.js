7be968ab8e9d726a3c7a94f54cf7c42618c5d30a
sakshamcapace-design <saksham.capace@gmail.com>
2026-07-19 11:06:10 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+    EMAILJS_PUBLIC_KEY: "IFY53T3g2b-pf--cX",
+    EMAILJS_SERVICE_ID: "service_0acmy5g",
+    EMAILJS_TEMPLATE_ID: "template_kdz5zcg",
+    WEB3FORMS_KEY: "6a34d81f-bad6-4835-9787-05a3a4a1ef9f"
+};
